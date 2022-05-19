94f4505b6fb810ef0a6837c59039dfc6bb77ba6a
ahmedalany7 <105869737+ahmedalany7@users.noreply.github.com>
2022-05-19 17:50:36 +0300

    Updated the Fibonacci function to start with 0

--- alani.cpp
+++ alani.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int fib(int n) {
 	if (n <= 1)
-		return n;
+		return abs(n-1);
 	return fib(n-1) + fib(n-2);
 }
 
 int main() {
 	cout << "Hello, World!" << endl;
-	cout << fib(10) << endl; 
+	cout << fib(2) << endl; 
 	return 0;
 }
